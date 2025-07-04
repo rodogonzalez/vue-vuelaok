2a29e298e23b58089171a9075b507aa9a778f2c5
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-07-04 10:42:01 -0600

    update

--- resources/js/components/dashboard/Country.vue
+++ resources/js/components/dashboard/Country.vue
@@ -61,7 +61,7 @@ onMounted(() => {
 
 
             <div class="col" v-for="video in country.item.video_links">
-                <iframe width="560" height="315" :src=video title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="560" height="315" :src="video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 {{video}}
 
             </div>
